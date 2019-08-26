15bab1ebe520e5badf8b76a82c7284824cb0382a
Dennis Fransen <dennisfransen@hotmail.se>
2019-08-27 00:42:10 +0200

    added styling to show screen

--- src/components/RestaurantsList.js
+++ src/components/RestaurantsList.js
@@ -4,6 +4,10 @@ import { withNavigation } from 'react-navigation'
 import RestaurantsDetail from '../components/RestaurantsDetail'
 
 const RestaurantsList = ({ title, restaurants, navigation }) => {
+    if (!restaurants.length) {
+        return null
+    }
+
     return (
         <View style={styles.container}>
             <Text style={styles.titleStyle}>{title}</Text>
--- src/screens/RestaurantsShowScreen.js
+++ src/screens/RestaurantsShowScreen.js
@@ -20,8 +20,11 @@ const RestaurantsShowScreen = ({ navigation }) => {
     }
 
     return (
-        <View>
-            <Text>{restaurant.name}</Text>
+        <View style={styles.container}>
+            <Text style={styles.titleStyle}>{restaurant.name}</Text>
+            <Text style={styles.textStyle}>Address: {restaurant.location.address1}</Text>
+            <Text style={styles.textStyle}>City: {restaurant.location.city}, {restaurant.location.zip_code}</Text>
+            <Text style={styles.textStyle}>Phone: {restaurant.phone}</Text>
             <FlatList
                 data={restaurant.photos}
                 keyExtractor={(photo) => photo}
@@ -34,9 +37,26 @@ const RestaurantsShowScreen = ({ navigation }) => {
 }
 
 const styles = StyleSheet.create({
+    container: {
+        margin: 10,
+        flex: 1
+    },
+    titleStyle: {
+        alignSelf: "center",
+        fontSize: 22,
+        fontWeight: "bold",
+        marginBottom: 10
+    },
+    textStyle: {
+        fontSize: 16,
+        marginBottom: 8
+    },
     imageStyle: {
-        height: 200,
-        width: 300
+        alignSelf: "center",
+        borderRadius: 5,
+        height: 300,
+        marginBottom: 8,
+        width: "100%",
     }
 })
 
