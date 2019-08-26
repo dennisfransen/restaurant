9b70dd8fea565d03f8582f3b0407b3c2804f4da2
Dennis Fransen <dennisfransen@hotmail.se>
2019-08-26 22:47:40 +0200

    setting up component for restaurants

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 import SearchBar from '../components/SearchBar'
 import useRestaurants from '../hooks/useRestaurants'
+import RestaurantsList from '../components/RestaurantsList'
 
 const SearchScreen = () => {
     const [term, setTerm] = useState('')
@@ -14,8 +15,14 @@ const SearchScreen = () => {
                 onTermChange={setTerm} 
                 onTermSubmit={() => searchApi(term)}
             />
+
             { errorMessage ? <Text>{errorMessage}</Text> : null }
+
             <Text>Found {restaurants.length} results</Text>
+
+            <RestaurantsList title="Cheap" />
+            <RestaurantsList title="Medium" />
+            <RestaurantsList title="Expensive" />
         </View>
     )
 }
